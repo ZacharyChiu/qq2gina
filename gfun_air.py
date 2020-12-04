d884bc5952f98d29cfd609f2275e17e262311610
Zachary <49262285+ZacharyChiu@users.noreply.github.com>
2020-12-04 23:49:46 +0800

    Update gfun_air.py
    Add: [tags] order

--- gfun_air.py
+++ gfun_air.py
@@ -4,8 +4,9 @@
 
 import os
 import pygame
-from mutagen.mp3 import MP3
+# from mutagen.mp3 import MP3
 import time
+import requests
 
 
 def play_music(path,f=16000):
@@ -13,7 +14,34 @@ def play_music(path,f=16000):
     pygame.mixer.music.load(path)
     pygame.mixer.music.play()
 
+def clean(l):
+    img = 'jpg.png.gif.jpeg.webp.jfif'.split('.')
+    new = []
+    for i in l:
+        if '.' in i:
+            type = i.split('.')[-1].lower()
+        else:
+            type = ''
+        if type in img:
+            new.append(i)
+    return new
 
+def sd_p_msg(qq,msg):
+	url = 'http://127.0.0.1:5700/send_private_msg'
+	params = {
+		"user_id": qq,
+		"message": msg}
+	response = requests.post(url, data=json.dumps(params), headers={'Content-Type': 'application/json'})
+	print(response.text)
+	return response
+def sd_g_msg(qq,msg):
+	url = 'http://127.0.0.1:5700/send_group_msg'
+	params = {
+		"group_id": qq,
+		"message": msg}
+	response = requests.post(url, data=json.dumps(params), headers={'Content-Type': 'application/json'})
+	print(response.text)
+	return response
 
 def do(ask):
     try:
@@ -41,10 +69,68 @@ def do(ask):
                         audio = MP3(path)
                         length = float(audio.info.length)
                         time.sleep(length)
-        elif ask == 'tem':
-            return 'emm...'
-        elif ask == 'cxk':
-            os.system('start C:/quickstart/gg.lnk ' + 'http://cxkv2.xyz/auth/register')
+        elif ask[:3] == 'tag':
+            path = '/media/pi/3312-1D19/Gidata/Source/Arts/Images/pic'
+            datapath = '~/python/qq2gina/data/images'
+            l = clean(os.listdir(path))
+            order = ask[4:].split(' ')
+            if len(order) ==1:
+                try:
+                    i = int(order[0])
+                    asknum = ask[4:]
+                    for f in l:
+                        if '.'+asknum+'.' in f:
+                            result = f
+                    # print(l)
+                    os.system('cp ' + path + '/' + result + ' ' + datapath)
+                    return '[CQ:image,file=' + result+']'
+                except:
+                    if order[0] == 'len':
+                        return str(len(l))
+                    else:
+                        # and mode (with just 1 tag)
+                        collection = []
+                        for f in l:
+                            if order[0]+'.' in f:
+                                collection.append(f.split('.')[-2])
+                        if collection:
+                            if len(collection) < 8:
+                                return '\n'.join(collection)
+                            else:
+                                re = ''
+                                for i in range(0,len(collection),5):
+                                    re = re + ' '.join(collection[i:i+5]) + '\n'
+                                return re
+                                    
+                        else:
+                            return 'Nothing here, honey.'
+                        pass
+            else:
+                if order[0] == '+':
+                    filelist = l[:]
+                    tags = order[1].split('.')
+                    clist = []
+                    for tag in tags:
+                        for filename in filelist:
+                            filetag = filename.split('.')
+                            if tag in filetag:
+                                clist.append(filename)
+                        filelist = clist[:]
+                        clist = []
+                    collection = []
+                    for i in filelist:
+                        collection.append(i.split('.')[-2])
+                    if collection:
+                        if len(collection) < 8:
+                            return '\n'.join(collection)
+                        else:
+                            re = ''
+                            for i in range(0,len(collection),5):
+                                re = re + ' '.join(collection[i:i+5]) + '\n'
+                            return re
+                    
+        elif ask[:2] == 'os':
+            return os.popen(ask[3:]).read()
         elif ask == 'bili':
             os.system('start C:/quickstart/gg.lnk ' + 'http://www.bilibili.com')
         else:
