ffadcc2c670df1ee95f5e17b3ca542860fd8d565
Zachary <49262285+ZacharyChiu@users.noreply.github.com>
2020-12-31 16:28:36 +0800

    Update qq2gina.py

--- qq2gina.py
+++ qq2gina.py
@@ -5,172 +5,242 @@
 '''
 import os 
 import re 
-import datetime
-import csv
-import pygame
-import requests
-import json
-import chatme
+import time
+# import csv
+# import pygame
+
+# 自建库
+import spider
 import gfun_air
-# import Ali_TTS as tts
 
-def play_music(path,f=16000):
-    pygame.mixer.init(frequency=f)
-    pygame.mixer.music.load(path)
-    pygame.mixer.music.play()
+# def play_music(path,f=16000):
+    # pygame.mixer.init(frequency=f)
+    # pygame.mixer.music.load(path)
+    # pygame.mixer.music.play()
     
-def kill_biaodian(s):
-    # 去掉所有标点
-    punc = '~`!#$%^&*()_+-=|\';“”:/.,?><~·！@#￥%……&*（）——+-=“：’；、。，？》《{}'
-    return re.sub(r"[%s]+" %punc, "",s)
+# def kill_biaodian(s):
+    # # 去掉所有标点
+    # punc = '~`!#$%^&*()_+-=|\';“”:/.,?><~·！@#￥%……&*（）——+-=“：’；、。，？》《{}'
+    # return re.sub(r"[%s]+" %punc, "",s)
 
-def have_speech(s):
-    # 检测日志中是否已经含有此语音文件，如果有，返回文件名编号
-    have = []
-    name = False
-    with open('tts_recoder.csv','r',encoding='utf-8') as f:
-        rows = f.readlines()[1:]
-    print('rows:',rows)
-    for b in rows:
-        body = b.split(',')[-1].strip()
-        have.append(kill_biaodian(body))
-    print('have:',have)
-    if kill_biaodian(s) in have:
-        index = have.index(kill_biaodian(s))
-        name = rows[index].split(',')[0]
-    return name
+# def have_speech(s):
+    # # 检测日志中是否已经含有此语音文件，如果有，返回文件名编号
+    # have = []
+    # name = False
+    # with open('tts_recoder.csv','r',encoding='utf-8') as f:
+        # rows = f.readlines()[1:]
+    # print('rows:',rows)
+    # for b in rows:
+        # body = b.split(',')[-1].strip()
+        # have.append(kill_biaodian(body))
+    # print('have:',have)
+    # if kill_biaodian(s) in have:
+        # index = have.index(kill_biaodian(s))
+        # name = rows[index].split(',')[0]
+    # return name
 
-def get_time():
-    cur_time = datetime.datetime.now()  # 数据类型为：datetime
-    now = str(cur_time).split('.')[0]
-    date = ''.join(now.split(' ')[0].split('-'))
-    time = ''.join(now.split(' ')[1].split(':'))
-    t = date + time
-    return t
-
-def get_msg():
-    logs = os.listdir('logs')
-    today_log = str(datetime.datetime.now()).split(' ')[0]+'.log'
-    if today_log in logs:
-        with open('logs\\'+today_log,'r',encoding='utf-8') as f:
-            log = f.read()
-        sd = re.findall(r'发送.*的消息.*.*',log)
-        rcv = re.findall(r'收到.*的消息.*.*',log)
-        return [rcv,sd]
-    else:
-        print('今天的日志特么没找到...')
-
-def sd_p_msg(qq,msg):
-	url = 'http://127.0.0.1:5700/send_private_msg'
-	params = {
-		"user_id": qq,
-		"message": msg}
-	response = requests.post(url, data=json.dumps(params), headers={'Content-Type': 'application/json'})
-	print(response.text)
-	return response
-def sd_g_msg(qq,msg):
-	url = 'http://127.0.0.1:5700/send_group_msg'
-	params = {
-		"group_id": qq,
-		"message": msg}
-	response = requests.post(url, data=json.dumps(params), headers={'Content-Type': 'application/json'})
-	print(response.text)
-	return response
-
-def what(text):
-    sh = re.findall(r'到.*的',text)
-    who = sh[0][1:-1]
-    id_ = who.split('(')[-1][:-2]
-    name = who.split('(')[0].split(' ')[-1]
-    msg = text.split(': ')[-1].strip()
-    return [name,id_,msg]
-# resp = sd_p_msg(1563382991,"Zac")
-# rsp = sd_msg(1039564640,"here")
-
-def readed(s):
-    with open('readed.txt','r',encoding='utf-8') as f:
-        readed = f.read()
-    if readed == s:
-        return True
-    else:
-        return False
-rcv_msg = []
-rcv = get_msg()[0]
-# for i in rcv:
-    # print('rcv:',i)
+def show_head():
+    # themsg来自全局变量
+    typedic = {'好友':'私聊','群':'群聊'}
+    msgtime = themsg.time
+    msgtype = typedic[themsg.msgtype]
+    date0 = msgtime.split(' ')[0].split('-')
+    date = date0[0][2:] + date0[1] + date0[2]
+    thetime = msgtime.split(' ')[1]
+    title_time = date + ' ' + thetime
+    title = '='*10 + msgtype + '(' + title_time + ')' + '='*10
     
-# if rcv != rcv_msg: # 有新消息
-    # rcv_msg = rcv[:]
-    # latest = rcv_msg[-1]
-    # content = what(latest)
-    # print(content)
-    # print('#%s#'%' '.join(content[-1].split(' ')[:-1]))
+    sdid = themsg.sdid
+    if themsg.sdid == '1563382991':
+        sdname = '超哥'
+    else:
+        sdname = themsg.sdname
+    if msgtype == '私聊':
+        vice_title = '$ ' + sdname + '(' + sdid + ')：[' + themsg.texttype + ']'
+        return title + '\n' + vice_title
+    elif msgtype == '群聊':
+        vice_title = themsg.sdqunname + '(' + themsg.sdqunid + ')\n$ ' + sdname + '(' + sdid + ')：[' + themsg.texttype + ']'
+        return title + '\n' + vice_title
 
-# os.system('start go-cqhttp-v0.9.32-windows-amd64.exe')
-
+# ===================================Main======================================
+did = 0
+qq = gfun_air.QQ()
+    
 while 1:
-    if rcv != rcv_msg: # 有新消息
-        
-        rcv_msg = rcv[:]
-        latest = rcv_msg[-1]
-        content = what(latest)
-        if '群' not in latest:
-            # 个人消息
-            print('#%s#'%' '.join(content[-1].split(' ')[:-1]))
-            print('='*20)
-            if not readed(content[-1]):
-                if content[1] == '1563382991':
-                    # 来自Zac
-                    if content[-1][:4] == 'gg$ ':
-                        order = ' '.join(content[-1].split('gg$ ')[-1].split(' ')[:-1])
-                        print('控制台：%s'%order)
-                        if order[:4] == 'sys ':
-                            os.system(order.split('sys ')[-1])
+    try:
+        Msgs = gfun_air.MsgID()  # 信息类
+        status = gfun_air.Readed()
+        nolog1 = Msgs.nologs
+        if nolog1:
+            if not did:
+                print('未找到今天的日志。')
+                os.system('start go-cqhttp.exe')
+                did = 1
+        else:  # 有日志
+            Logs = Msgs.logs
+            latest = Msgs.rcv[-1]  # 最新消息
+            ed_msg = status.dic['readed']  # 最后的已读消息
+            if status.dic['status'] == 'sleep':  # 【离线状态，等待激活】
+                weborder = spider.web_order()
+                if weborder == 'Order:ONYihe':  # 要求姬娜上线(办公室端)
+                        os.system('start go-cqhttp.exe')
+                        status.write(status='normal')
+                        time.sleep(25)  # 等待加载
+                        qq.sd_p_msg(1563382991,'你的小姬姬已上线')
+                        
+            elif status.dic['status'] == 'normal':  # 【普通聊天状态】
+                if not status.readed(latest):  # 有未读消息
+                    themsg = gfun_air.MsgID(Msgs.get_id(latest))  # 最新消息独立信息类
+                    if themsg.msgtype == '好友':  # 私信
+                        print(show_head())
+                        print('#'+themsg.text+'#')
+                        if themsg.sdid == '1563382991':
+                            if themsg.texttype == '命令':
+                                order = themsg.order
+                                print('控制台：%s'%order)
+                                r = gfun_air.do(order)
+                                if r.split(' ')[0] == 'taskkill':  #准备杀了cqhttp
+                                    qq.sd_p_msg(1563382991,'可爱的姬娜会离你而去')
+                                    round = 1
+                                    while round:
+                                        if Msgs.update()[-1] != latest:  # 有新消息
+                                            Logs = Msgs.logs
+                                            latest = Logs[-1]
+                                            msgid = Msgs.get_id(latest)
+                                            themsg = gfun_air.MsgID(msgid)
+                                            if themsg.msgtype == '好友':
+                                                if themsg.sdid == '1563382991':
+                                                    if themsg.text in ['好的','yes']:
+                                                        qq.sd_p_msg(1563382991,'道不同不相为谋，我日你妈先告辞了[CQ:face,id=118]')
+                                                        os.system(r)
+                                                        status.update()
+                                                        status.write(text=themsg.text,status='sleep')
+                                                        round = 0
+                                                        print('-'*40)
+                                                        print('$ 姬娜:', '啊我死了', '\n')
+                                                        print('\n'*2)
+                                                    elif themsg.text in ['不了','no','取消','cancel','算了']:
+                                                        qq.sd_p_msg(1563382991,'那我就不下线啦~')
+                                                        status.update()
+                                                        status.write(text=themsg.text)
+                                                        round = 0
+                                                        print('-'*40)
+                                                        print('$ 姬娜:', '......', '\n')
+                                                        print('\n'*2)
+                            elif themsg.cq == 1:  # Zac的私聊CQ码
+                                status.update()
+                                status.write(text=latest)
+                                print('\n'*2)
+                            elif themsg.texttype == '普通对话':
+                                ans = gfun_air.gfun(themsg.text.strip())
+                                # print('###现在获取到回答：%s'%ans)
+                                print('-'*40)
+                                print('$ 姬娜:', ans, '\n')
+                                qq.sd_p_msg(1563382991,ans)
+                                status.update()  # 主要是状态可能变了，所以要更新
+                                status.write(text=latest)
+                                print('\n'*2)
+                                
+                    elif themsg.msgtype == '群':
+                        qunname = themsg.sdqunname
+                        qunnum = themsg.sdqunid
+                        saynum = themsg.sdid  # 发言人
+                        text = themsg.text
+                        print(show_head())
+                        print('#'+themsg.text+'#')
+                        if themsg.cq == 1:
+                            cqtype = themsg.cqtype
+                            cqfile = themsg.cqfile.split('=')[-1]
+                            # print('text: #%s#'%text)
+                            if themsg.cqtype == 'at':  # @某人
+                                if themsg.texttype == '@姬娜':  # @Gina
+                                    response = gfun_air.gfun(text.strip())
+                                    if themsg.text != '':
+                                        ans = '[CQ:at,qq='+saynum+'] ' + response
+                                        qq.sd_g_msg(qunnum,ans)
+                                        print('-'*40)
+                                        print('$ 姬娜:', ans, '\n')
+                                        status.update()
+                                        status.write(text=latest)
+                                        print('\n'*2)
+                                    else:
+                                        ans = '[CQ:at,qq='+saynum+'] ' + '干嘛？'
+                                        qq.sd_g_msg(qunnum,ans)
+                                        status.update()
+                                        status.write(text=latest)
+                                        round = 0
+                                        while round <5:  # 等待5轮对话，如果此人不发言，就不鸟他
+                                            if Msgs.update()[-1] != latest:  # 有新消息
+                                                Logs = Msgs.logs
+                                                latest = Logs[-1]
+                                                msgid = Msgs.get_id(latest)
+                                                themsg = gfun_air.MsgID(msgid)
+                                                if themsg.msgtype == '群':
+                                                    if themsg.sdid == saynum:
+                                                        if themsg.text != '':
+                                                            response = gfun_air.gfun(text.strip())
+                                                            ans = '[CQ:at,qq='+saynum+'] ' + response
+                                                            qq.sd_g_msg(qunnum,ans)
+                                                            status.update()
+                                                            status.write(text=latest)
+                                                            print('\n'*2)
+                                                            round = 6
+                                                    else:
+                                                        if themsg.cq == 1:
+                                                            cqtype = themsg.cqtype
+                                                            cqfile = themsg.cqfile.split('=')[-1]
+                                                            if themsg.cqtype == 'at':  # @某人
+                                                                if themsg.texttype == '@姬娜':  # @Gina
+                                                                    if themsg.text != '':
+                                                                        response = gfun_air.gfun(text.strip())
+                                                                        ans = '[CQ:at,qq='+themsg.sdid+'] ' + response
+                                                                        qq.sd_g_msg(qunnum,ans)
+                                                                        status.update()
+                                                                        status.write(text=latest)
+                                                                        print('\n'*2)
+                                                        else:
+                                                            status.update()
+                                                            status.write(text=latest)
+                                                        round += 1
+                                            # print('Round %d'%round)
+                                        status.update()
+                                        status.write(text=latest)
+                                                        
+                                        
+                                        
                         else:
-                            print('>>>获到超哥的【命令行】！')
-                            r = gfun_air.do(order)
-                            print('$ 姬娜:', r, '\n')
-                            sd_p_msg(1563382991,r)
-                            with open('readed.txt','w',encoding='utf-8') as f:
-                                f.write(content[-1])
-                            
-                    elif content[-1][:4] == '[CQ:':
-                        print('>>>捕获到【CQ码】！')
-                        
-                    else:
-                        print('>>>捕获到【普通对话】！')
-                        name = 'tts_logs\\'+str(get_time()) + '.mp3'
-                        ans = chatme.chat(' '.join(content[-1].split(' ')[:-1]))
-                        # ans = '...'
-                        print('$ 姬娜:', ans, '\n')
-                        sd_p_msg(1563382991,ans)
-                        with open('readed.txt','w',encoding='utf-8') as f:
-                            f.write(content[-1])
-                        
-                        # h = have_speech(ans)
-                        # if h:
-                            # play_music('tts_logs\\'+h+'.mp3',16000)
-                        # else:
-                            # tts.readit(ans,name)
-                            # play_music(name,16000)
-                            # with open('tts_recoder.csv','a',encoding='utf-8') as f:
-                                # writer = csv.writer(f)
-                                # writer.writerow([name.split('\\')[-1].split('.')[0],ans])
-        else:
-            #群消息
-            print(content)
-            print('收到群消息')
-            qun = re.findall(r'群.*内',latest)[0]
-            qunname = qun.split('(')[0]  # 群名
-            qunnum = qun.split('(')[-1].split(')')[0]  # 群号
-            saynum = content[1]  # 发言人
-            
-            if not readed(content[-1]):
-                ans = chatme.chat(' '.join(content[-1].split(' ')[:-1]))
-                sd_g_msg(qunnum,ans)
-                with open('readed.txt','w',encoding='utf-8') as f:
-                    f.write(content[-1])
-                # print(sayman)
-                print('$ 姬娜:', ans, '\n')
+                            if saynum == '1563382991':
+                                if text[-2:] == '  ':
+                                    ans = gfun_air.gfun(text.strip())
+                                    qq.sd_g_msg(qunnum,ans)
+                                    status.update()
+                                    status.write(text=latest)
+                                    print('-'*40)
+                                    print('$ 姬娜:', ans, '\n')
+                                    print('\n'*2)
+                                elif themsg.texttype == '命令':
+                                    order = text[4:].strip()
+                                    # print('#%s#'%order)
+                                    if order == 'test':
+                                        ans = '[CQ:record,file=test.amr]'
+                                        qq.sd_g_msg(qunnum,ans)
+                                else:
+                                    status.update()
+                                    status.write(text=latest)
+                            else:
+                                status.update()
+                                status.write(text=latest)
+
+        try:
+            if 'go-cqhttp.exe' not in gfun_air.whatrun():
+                if status.dic['status'] != 'sleep':
+                    status.write(status='sleep')
+                    print('已更新状态为sleep')
+        except Exception as e:
+            # print(e)
+            pass
     
-    rcv = get_msg()[0]
+        
+
+    except Exception as e:
+        print(e)
